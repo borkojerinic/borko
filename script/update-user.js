3fdc7f534bc3e1bb28af6c132f07b0b967c54a4f
Borko Jerinic <jerinicborko@gmail.com>
2022-04-18 12:36:20 +0200

    code changes

--- script/update-user.js
+++ script/update-user.js
@@ -1,8 +1,8 @@
 //#region Update user
 
-let path = '../borko/list-users.html';
-let url = 'http://dev.qposoft.com:4082/api/users';
-let myId = JSON.parse(localStorage.getItem('update-user-button'));
+const relativePathToListUsers = './list-users.html';
+const usersFromServerPath = 'http://dev.qposoft.com:4082/api/users';
+const myId = JSON.parse(localStorage.getItem('update-user-button'));
 let nameBeforeUpdate = '';
 let emailBeforeUpdate = '';
 
@@ -10,7 +10,7 @@ document.body.addEventListener('load', userData(myId));
 
 function userData(id) {
     $.ajax({
-        url: url + '/' + id,
+        url: usersFromServerPath + '/' + id,
         type: 'GET',
         success: function (response) {
             setInputForUser(response.data);
@@ -19,36 +19,45 @@ function userData(id) {
 }
 
 function setInputForUser(userData) {
-    nameBeforeUpdate = document.getElementById('update-name').value = userData.name;
-    emailBeforeUpdate = document.getElementById('update-email').value = userData.email;
+    nameBeforeUpdate = document.getElementById('name').value = userData.name;
+    document.title = nameBeforeUpdate;
+    emailBeforeUpdate = document.getElementById('email').value = userData.email;
 }
 
 function updateUser() {
-    document.getElementById('message-update').innerHTML = '';
-    let name = document.getElementById('update-name').value;
-    let email = document.getElementById('update-email').value;
+    document.getElementById('error-message').innerHTML = '';
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
     let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (email.match(validEmail)) {
-        $.ajax({
-            url: `${url}/${myId}?name=${name}&email=${email}`,
-            type: 'PUT',
-            success: function (response) {
-                location.href = path;
-            },
-            error: function (request, status, error) {
-                document.getElementById('error-success-update').style.visibility = 'visible';
-                document.getElementById('message-update').innerHTML = JSON.parse(request.responseText).errors.name;
+        if (nameBeforeUpdate !== name || emailBeforeUpdate !== email) {
+            const result = confirm("Want to update user " + nameBeforeUpdate + "?");
+            if (result) {
+                $.ajax({
+                    url: `${usersFromServerPath}/${myId}?name=${name}&email=${email}`,
+                    type: 'PUT',
+                    success: function (response) {
+                        location.href = relativePathToListUsers;
+                    },
+                    error: function (request, status, error) {
+                        document.getElementById('message').style.visibility = 'visible';
+                        document.getElementById('error-message').innerHTML = JSON.parse(request.responseText).errors.name;
+                    }
+                });
             }
-        });
+        } else {
+            document.getElementById('message').style.visibility = 'visible';
+            document.getElementById('error-message').innerHTML = 'Unchanged form!';
+        }
     } else {
-        document.getElementById('error-success-update').style.visibility = 'visible';
-        document.getElementById('message-update').innerHTML = 'Invalid email!';
+        document.getElementById('message').style.visibility = 'visible';
+        document.getElementById('error-message').innerHTML = 'Invalid email!';
     }
 }
 
 document.getElementById('go-back').onclick = function () {
-    location.href = path;
+    location.href = relativePathToListUsers;
 }
 
 //#endregion
